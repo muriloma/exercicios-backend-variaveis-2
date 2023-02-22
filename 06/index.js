8024b7345b333e2a96a3321c1a75becc4a4dd5fd
Murilo M Andrade <andrade.murilom@gmail.com>
2023-02-22 18:24:28 -0300

    Melhora da resposta, alteração no checklist do README

--- 06/index.js
+++ 06/index.js
@@ -1,4 +1,5 @@
 let distancia = 500, tempo = 10;
-let velocidadeMs = distancia / tempo
-let velocidadeKmh = velocidadeMs * 3.6
-console.log(velocidadeKmh)
+let velocidadeMs = distancia / tempo;
+let velocidadeKmh = velocidadeMs * 3.6;
+console.log(`Dada uma distancia de ${distancia} metros, percorrida em ${tempo} segundos`);
+console.log(`A velocidade média calculada é de ${velocidadeKmh}Km/h`);
