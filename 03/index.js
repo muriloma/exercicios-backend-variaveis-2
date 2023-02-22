0430a7b7041785fb3a83b11f93a9e85d13154bdf
Murilo M Andrade <andrade.murilom@gmail.com>
2023-02-22 16:42:51 -0300

    Aprimorando a resposta e preenchendo o checklist do README

--- 03/index.js
+++ 03/index.js
@@ -3,4 +3,4 @@ let tenis = 129.99;
 let valorDesconto = tenis - dinheiro;
 let taxaDesconto = (valorDesconto * 100) / tenis
 taxaDesconto = taxaDesconto.toFixed(0)
-console.log(taxaDesconto)
+console.log(`Para a compra do tenis seria necessário um desconto de aproximadamente ${taxaDesconto}%`)
