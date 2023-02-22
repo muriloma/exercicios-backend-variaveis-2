856a21a03f76b2dd3c8fc2240c5fb64e0b65aba1
Murilo M Andrade <andrade.murilom@gmail.com>
2023-02-22 15:50:48 -0300

    Inserida formula de IMC e resposta do calculo para variaveis definidas de peso e altura

--- 01/index.js
+++ 01/index.js
@@ -1,2 +1,4 @@
 // Inserindo as variáveis de peso e altura
 let peso = 69, altura = 1.69;
+let imc = peso / (altura ** 2);
+console.log(`Para uma altura de ${altura}m e um peso de ${peso}kg o IMC calculado é de: ${imc}`);
