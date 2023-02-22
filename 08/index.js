5e33a2c0749b05f82d9a4cfabaf244f3767f057d
Murilo M Andrade <andrade.murilom@gmail.com>
2023-02-22 19:11:17 -0300

    Resolvido o problema e alterado o checklist do README

---
+++ 08/index.js
@@ -0,0 +1,5 @@
+// comprimento e area
+let raio = 1;
+let area = Math.PI * (raio ** 2), comprimento = 2 * Math.PI * raio;
+comprimento = comprimento.toFixed(2), area = area.toFixed(2);
+console.log(`Para um raio de ${raio} o comprimento é ${comprimento} e a área ${area}`);
