9b8e661cd981399b82d34e1caf57efcc3a1645ed
Murilo M Andrade <andrade.murilom@gmail.com>
2023-02-22 15:52:27 -0300

    adicionando metodo toFixed para resultado com duas casas decimais

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,5 @@
 // Inserindo as variáveis de peso e altura
 let peso = 69, altura = 1.69;
 let imc = peso / (altura ** 2);
+imc = imc.toFixed(2)
 console.log(`Para uma altura de ${altura}m e um peso de ${peso}kg o IMC calculado é de: ${imc}`);
